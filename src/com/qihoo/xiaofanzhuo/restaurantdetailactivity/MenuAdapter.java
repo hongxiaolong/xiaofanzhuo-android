143a19c3624bde7be3d5fc1a1ae9908300f7f865
xiaolonginfo <vincent_hong2012@hotmail.com>
2014-05-27 16:47:58 +0800

    2014-05-27 16:47 by hongxiaolong 增加购物车动画效果。

--- src/com/qihoo/xiaofanzhuo/restaurantdetailactivity/MenuAdapter.java
+++ src/com/qihoo/xiaofanzhuo/restaurantdetailactivity/MenuAdapter.java
@@ -3,92 +3,129 @@ package com.qihoo.xiaofanzhuo.restaurantdetailactivity;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.carrey.bitmapcachedemo.R;
-import com.example.android.bitmapfun.util.ImageFetcher;
-import com.qihoo.xiaofanzhuo.datafromserver.BusinessData.MenuData;
-
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MenuAdapter  extends BaseAdapter {
-	
+import com.carrey.bitmapcachedemo.R;
+import com.example.android.bitmapfun.util.ImageFetcher;
+import com.qihoo.xiaofanzhuo.datafromserver.BusinessData.MenuData;
+
+public class MenuAdapter extends BaseAdapter {
+
 	private static final String TAG = "MenuAdapter";
-	
+
 	private Context mContext;
-    private List<MenuData> mInfos;
-    private ImageFetcher mImageFetcher;
+	private List<MenuData> mInfos;
+	private ImageFetcher mImageFetcher;
 
-    public MenuAdapter(Context context, ImageFetcher f) {
-        mInfos = new ArrayList<MenuData>();
-        mImageFetcher = f;
-    }
+	public MenuAdapter(Context context, ImageFetcher f) {
+		mInfos = new ArrayList<MenuData>();
+		mContext = context;
+		mImageFetcher = f;
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
 
-        ViewHolder holder;
-        
-        String infoUrl= mInfos.get(position).getFoodImgUrl().toString();
-		String infoName = mInfos.get(position).getFood().toString();
-		String infoPrice = mInfos.get(position).getFoodPrice().toString();
+		ViewHolder holder;
 
-        if (convertView == null) {
-            LayoutInflater layoutInflator = LayoutInflater.from(parent.getContext());
-            convertView = layoutInflator.inflate(R.layout.hong_infos_list, null);
-            holder = new ViewHolder();
-            holder.imageView = (ImageView) convertView.findViewById(R.id.news_pic);
-            holder.contentView = (TextView) convertView.findViewById(R.id.news_title);
-            convertView.setTag(holder);
-        }
-        holder = (ViewHolder) convertView.    getTag();
+		final String infoUrl = mInfos.get(position).getFoodImgUrl().toString();
+		final String infoName = mInfos.get(position).getFood().toString();
+		final String infoPrice = mInfos.get(position).getFoodPrice().toString();
 
-        
-//        float iHeight = ((float) 200 / 183 * duitangInfo.getHeight());
-        holder.imageView.setLayoutParams(new LinearLayout.LayoutParams(
-        		LinearLayout.LayoutParams.MATCH_PARENT,
-        		(int) 150));
+		if (convertView == null) {
+			LayoutInflater layoutInflator = LayoutInflater.from(parent
+					.getContext());
+			convertView = layoutInflator
+					.inflate(R.layout.hong_infos_list, null);
+			holder = new ViewHolder();
+			holder.imageView = (ImageView) convertView
+					.findViewById(R.id.news_pic);
+			holder.checkBox = (CheckBox) convertView
+					.findViewById(R.id.pict_checkbox);
+			holder.contentView = (TextView) convertView
+					.findViewById(R.id.news_title);
+			convertView.setTag(holder);
+		}
+		holder = (ViewHolder) convertView.getTag();
 
-        holder.contentView.setText(infoName + " " + infoPrice);
-        mImageFetcher.loadImage(infoUrl, holder.imageView);
-        return convertView;
-    }
+		// float iHeight = ((float) 200 / 183 * duitangInfo.getHeight());
+		holder.imageView.setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT, (int) 150));
 
-    class ViewHolder {
-        ImageView imageView;
-        TextView contentView;
-        TextView timeView;
-    }
+		holder.contentView.setText(infoName + " " + infoPrice);
+		mImageFetcher.loadImage(infoUrl, holder.imageView);
 
-    @Override
-    public int getCount() {
-        return mInfos.size();
-    }
+		final CheckBox checkBox = holder.checkBox;
+		holder.imageView.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				SharedPreferences mySharedPreferences = mContext.getSharedPreferences(
+						"MenuOrder", 
+						mContext.CONTEXT_IGNORE_SECURITY);
+				SharedPreferences.Editor editor = mySharedPreferences.edit();
+                String url = mySharedPreferences.getString("Url", "");
+                String name = mySharedPreferences.getString("Name", "");
+                String price = mySharedPreferences.getString("Price", "");
+                String amount = mySharedPreferences.getString("amount", "");
+				editor.putString("Url", url + "_" + infoUrl);
+				editor.putString("Name", name + "_" + infoName);
+				editor.putString("Price", price + "_" + infoPrice);
+				editor.putString("amount", amount + "_" + "1");
+				editor.commit();
+				
+				// 使用toast信息提示框提示成功写入数据
+				Log.i(TAG, "写入SharedPreferences:" + infoName + " " + infoPrice + " " + infoUrl);
+			}
 
-    @Override
-    public Object getItem(int arg0) {
-        return mInfos.get(arg0);
-    }
+		});
 
-    @Override
-    public long getItemId(int arg0) {
-        return 0;
-    }
+		return convertView;
+	}
 
-    public void addItemLast(List<MenuData> datas) {
-		 for (int i = 0; i < datas.size(); ++i) 
-	            mInfos.add(datas.get(i));
-   }
+	class ViewHolder {
+		ImageView imageView;
+		CheckBox checkBox;
+		TextView contentView;
+		TextView timeView;
+	}
 
-   public void addItemTop(List<MenuData> datas) {
-       for (int i = 0; i < datas.size(); ++i) {
-           mInfos.add(datas.get(i));
-       }
-   }
+	@Override
+	public int getCount() {
+		return mInfos.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return mInfos.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return 0;
+	}
+
+	public void addItemLast(List<MenuData> datas) {
+		for (int i = 0; i < datas.size(); ++i)
+			mInfos.add(datas.get(i));
+	}
+
+	public void addItemTop(List<MenuData> datas) {
+		for (int i = 0; i < datas.size(); ++i) {
+			mInfos.add(datas.get(i));
+		}
+	}
 
 }
--- src/com/qihoo/xiaofanzhuo/restaurantdetailactivity/MenuMainActivity.java
+++ src/com/qihoo/xiaofanzhuo/restaurantdetailactivity/MenuMainActivity.java
@@ -1,10 +1,30 @@
 package com.qihoo.xiaofanzhuo.restaurantdetailactivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.carrey.bitmapcachedemo.R;
 import com.example.android.bitmapfun.util.ImageFetcher;
@@ -12,12 +32,10 @@ import com.origamilabs.library.views.StaggeredGridView;
 import com.qihoo.xiaofanzhuo.datafromserver.BusinessData;
 import com.qihoo.xiaofanzhuo.datafromserver.BusinessData.MenuData;
 
-/** This will not work so great since the heights of the imageViews are
- * calculated on the iamgeLoader callback ruining the offsets. To fix this try
- * to get the (intrinsic) image width and height and set the views height
- * manually. I will look into a fix once I find extra time.
- * 
- * @author Maurycy Wojtowicz */
+/*
+ * MenuMainActivity
+ * by hongxiaolong
+ */
 public class MenuMainActivity extends Activity {
 	
 	private ImageFetcher mImageFetcher;
@@ -26,11 +44,25 @@ public class MenuMainActivity extends Activity {
 	private ArrayList<MenuData> mMenuDataList;
 	
 	private MenuAdapter mAdapter;
+	
+	private ImageView shopCart;//购物车
+	private ViewGroup anim_mask_layout;//动画层
+	private ImageView buyImg;// 这是在界面上跑的小图片
+	private int buyNum = 0;//购买数量
+	private BadgeView buyNumView;//显示购买数量的控件
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hong_activity_main);
+		
+		SharedPreferences mySharedPreferences =getSharedPreferences("MenuOrder",PreferenceActivity.MODE_WORLD_WRITEABLE);
+		SharedPreferences.Editor editor = mySharedPreferences.edit();
+		editor.putString("Url", "");
+		editor.putString("Name", "");
+		editor.putString("Price", "");
+		editor.putString("amount", "");
+		editor.commit();
 		
 		Bundle extras = getIntent().getExtras();
 		mExtraDatas = extras.getString("data");
@@ -53,6 +85,7 @@ public class MenuMainActivity extends Activity {
 		mAdapter.addItemLast(mMenuDataList);
 		mAdapter.notifyDataSetChanged();
 
+		shopCartInit();
 	}
 
 
@@ -60,6 +93,216 @@ public class MenuMainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	private void shopCartInit() {
+		shopCart = (ImageView) findViewById(R.id.shopping_img_cart);
+		buyNumView = new BadgeView(MenuMainActivity.this, shopCart);
+		buyNumView.setTextColor(Color.WHITE);
+		buyNumView.setBackgroundColor(Color.RED);
+		buyNumView.setTextSize(12);
+	}
+	
+	/**
+	 * @Description: 创建动画层
+	 * @param
+	 * @return void
+	 * @throws
+	 */
+	private ViewGroup createAnimLayout() {
+		ViewGroup rootView = (ViewGroup) this.getWindow().getDecorView();
+		LinearLayout animLayout = new LinearLayout(this);
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.MATCH_PARENT);
+		animLayout.setLayoutParams(lp);
+		animLayout.setId(Integer.MAX_VALUE);
+		animLayout.setBackgroundResource(android.R.color.transparent);
+		rootView.addView(animLayout);
+		return animLayout;
+	}
+
+	private View addViewToAnimLayout(final ViewGroup vg, final View view,
+			int[] location) {
+		int x = location[0];
+		int y = location[1];
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		lp.leftMargin = x;
+		lp.topMargin = y;
+		view.setLayoutParams(lp);
+		return view;
+	}
+
+	private void setAnim(final View v, int[] start_location) {
+		anim_mask_layout = null;
+		anim_mask_layout = createAnimLayout();
+		anim_mask_layout.addView(v);//把动画小球添加到动画层
+		final View view = addViewToAnimLayout(anim_mask_layout, v,
+				start_location);
+		int[] end_location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
+		shopCart.getLocationInWindow(end_location);// shopCart是那个购物车
+
+		// 计算位移
+		int endX = 0 - start_location[0] + 40;// 动画位移的X坐标
+		int endY = end_location[1] - start_location[1];// 动画位移的y坐标
+		TranslateAnimation translateAnimationX = new TranslateAnimation(0,
+				endX, 0, 0);
+		translateAnimationX.setInterpolator(new LinearInterpolator());
+		translateAnimationX.setRepeatCount(0);// 动画重复执行的次数
+		translateAnimationX.setFillAfter(true);
+
+		TranslateAnimation translateAnimationY = new TranslateAnimation(0, 0,
+				0, endY);
+		translateAnimationY.setInterpolator(new AccelerateInterpolator());
+		translateAnimationY.setRepeatCount(0);// 动画重复执行的次数
+		translateAnimationX.setFillAfter(true);
+
+		AnimationSet set = new AnimationSet(false);
+		set.setFillAfter(false);
+		set.addAnimation(translateAnimationY);
+		set.addAnimation(translateAnimationX);
+		set.setDuration(800);// 动画的执行时间
+		view.startAnimation(set);
+		// 动画监听事件
+		set.setAnimationListener(new AnimationListener() {
+			// 动画的开始
+			@Override
+			public void onAnimationStart(Animation animation) {
+				v.setVisibility(View.VISIBLE);
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+			}
+
+			// 动画的结束
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				v.setVisibility(View.GONE);
+				buyNum++;//让购买数量加1
+				buyNumView.setText(buyNum + "");//
+				buyNumView.setBadgePosition(BadgeView.POSITION_TOP_RIGHT);
+				buyNumView.show();
+			}
+		});
+
+	}
+	
+	public class MenuAdapter extends BaseAdapter {
+
+		private static final String TAG = "MenuAdapter";
+
+		private Context mContext;
+		private List<MenuData> mInfos;
+		private ImageFetcher mImageFetcher;
+
+		public MenuAdapter(Context context, ImageFetcher f) {
+			mInfos = new ArrayList<MenuData>();
+			mContext = context;
+			mImageFetcher = f;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			ViewHolder holder;
+
+			final String infoUrl = mInfos.get(position).getFoodImgUrl().toString();
+			final String infoName = mInfos.get(position).getFood().toString();
+			final String infoPrice = mInfos.get(position).getFoodPrice().toString();
+
+			if (convertView == null) {
+				LayoutInflater layoutInflator = LayoutInflater.from(parent
+						.getContext());
+				convertView = layoutInflator
+						.inflate(R.layout.hong_infos_list, null);
+				holder = new ViewHolder();
+				holder.imageView = (ImageView) convertView
+						.findViewById(R.id.news_pic);
+				holder.checkBox = (CheckBox) convertView
+						.findViewById(R.id.pict_checkbox);
+				holder.contentView = (TextView) convertView
+						.findViewById(R.id.news_title);
+				convertView.setTag(holder);
+			}
+			holder = (ViewHolder) convertView.getTag();
+
+			// float iHeight = ((float) 200 / 183 * duitangInfo.getHeight());
+			holder.imageView.setLayoutParams(new LinearLayout.LayoutParams(
+					LinearLayout.LayoutParams.MATCH_PARENT, (int) 150));
+
+			holder.contentView.setText(infoName + " " + infoPrice);
+			mImageFetcher.loadImage(infoUrl, holder.imageView);
+
+			final CheckBox checkBox = holder.checkBox;
+			holder.imageView.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					
+					int[] start_location = new int[2];// 一个整型数组，用来存储按钮的在屏幕的X、Y坐标
+					v.getLocationInWindow(start_location);// 这是获取购买按钮的在屏幕的X、Y坐标（这也是动画开始的坐标）
+					buyImg = new ImageView(mContext);// buyImg是动画的图片，我的是一个小球（R.drawable.sign）
+					buyImg.setImageResource(R.drawable.hong_sign);// 设置buyImg的图片
+					setAnim(buyImg, start_location);// 开始执行动画
+					
+					SharedPreferences mySharedPreferences = mContext.getSharedPreferences(
+							"MenuOrder", 
+							mContext.CONTEXT_IGNORE_SECURITY);
+					SharedPreferences.Editor editor = mySharedPreferences.edit();
+	                String url = mySharedPreferences.getString("Url", "");
+	                String name = mySharedPreferences.getString("Name", "");
+	                String price = mySharedPreferences.getString("Price", "");
+	                String amount = mySharedPreferences.getString("amount", "");
+					editor.putString("Url", url + "_" + infoUrl);
+					editor.putString("Name", name + "_" + infoName);
+					editor.putString("Price", price + "_" + infoPrice);
+					editor.putString("amount", amount + "_" + "1");
+					editor.commit();
+					
+					// 使用toast信息提示框提示成功写入数据
+					Log.i(TAG, "写入SharedPreferences:" + name + " " + price + " " + " " + amount + url);
+				}
+
+			});
+
+			return convertView;
+		}
+
+		class ViewHolder {
+			ImageView imageView;
+			CheckBox checkBox;
+			TextView contentView;
+			TextView timeView;
+		}
+
+		@Override
+		public int getCount() {
+			return mInfos.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			return mInfos.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return 0;
+		}
+
+		public void addItemLast(List<MenuData> datas) {
+			for (int i = 0; i < datas.size(); ++i)
+				mInfos.add(datas.get(i));
+		}
+
+		public void addItemTop(List<MenuData> datas) {
+			for (int i = 0; i < datas.size(); ++i) {
+				mInfos.add(datas.get(i));
+			}
+		}
+
 	}
 
 }
